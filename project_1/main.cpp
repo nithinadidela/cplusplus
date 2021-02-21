d637281fe7096b27c91d870903a4bd58fb10966d
Nithin Adidela <nithinadidela@gmail.com>
2021-02-21 17:35:58 +0530

    the input and output is working (#2)

---
+++ project_1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Enter your favourite number between 1 and 100: ";
+  int favourite_number;
+  std::cin >> favourite_number;
+  std::cout << "Amazing! That's my favourite number too!" << std::endl;
+  return 0;
+}
